a240ec6d685688448bcfb4b4c9616daa303ad76d
Noah Hendrickson <nhendrickson@backtrace.io>
2020-08-01 23:41:52 -0400

    jellyfin: updated module, added PreExec step to service to allow saving db versions due to breakage in upgrade on 10.6.0.

--- nixos/modules/services/misc/jellyfin.nix
+++ nixos/modules/services/misc/jellyfin.nix
@@ -35,7 +35,26 @@ in
         Group = cfg.group;
         StateDirectory = "jellyfin";
         CacheDirectory = "jellyfin";
-        ExecStart = "${pkgs.jellyfin}/bin/jellyfin --datadir '/var/lib/${StateDirectory}' --cachedir '/var/cache/${CacheDirectory}'";
+        db_version = pkgs.lib.versions.majorMinor pkgs.jellyfin.version;
+        ExecPreStart = ''
+          #!/bin/bash
+          mkdir -p /var/lib/${StateDirectory}/database/${db_version}
+          if [ "${db_version}" = "10.6.1" ]; then
+            cp -r /var/lib/${StateDirectory}/data /var/lib/${StateDirectory}/database/${db_version}/
+            exit 0
+          else
+            versions=$(find /var/lib/${StateDirectory}/database/ -maxdepth 1 -mindepth 1 | sort -r)
+            for version in versions;
+            do
+              if [ -d "$version" ]; then
+                if find "$version" -mindepth 1 -print -quit 2>/dev/null | grep -q .; then;
+                  cp -r "$version" /var/lib/${StateDirectory}/database/${db_version}
+                  break
+                fi
+              fi
+            done
+        '';
+        ExecStart = "${pkgs.jellyfin}/bin/jellyfin --datadir '/var/lib/${StateDirectory}/database/${db_version}' --cachedir '/var/cache/${CacheDirectory}'";
         Restart = "on-failure";
       };
     };
